1ddfede4436b03afde0a02a53b98f9ba7fb44fba
Anand Kumar <karanianandkumar@yahoo.com>
2018-04-24 23:24:27 +0530

    get count of documents

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,10 +9,20 @@ MongoClient.connect('mongodb://localhost:27017/',(err,client)=>{
     }
 
     var db=client.db('TodoApp');
-    db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
+    db.collection('Todos').find({
+        _id:new ObjectID('5adf65a0e09a520346da8ce1')
+    }).toArray().then((docs)=>{
         console.log('Todos');
 
         console.log(JSON.stringify(docs,undefined,2));
+    },(err)=>{
+        console.log("Unable to load Todos");
+    });
+
+    db.collection('Todos').find().count().then((count)=>{
+        console.log('Todos');
+
+        console.log("Todos count ",count);
     },(err)=>{
         console.log("Unable to load Todos");
     });
