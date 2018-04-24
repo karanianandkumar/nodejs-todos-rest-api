36e5113327697308bfd244d376ac34e71d269271
Anand Kumar <karanianandkumar@yahoo.com>
2018-04-24 23:17:58 +0530

    find documents using conditions

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,22 @@
-const MongoClient=require("mongodb").MongoClient;
+// const MongoClient=require("mongodb").MongoClient;
+
+const {MongoClient,ObjectID}=require("mongodb");
 
 MongoClient.connect('mongodb://localhost:27017/',(err,client)=>{
     if(err){
         console.log("Unable to connect to Mongo DB server");
         return;
     }
+
     var db=client.db('TodoApp');
+    db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
+        console.log('Todos');
+
+        console.log(JSON.stringify(docs,undefined,2));
+    },(err)=>{
+        console.log("Unable to load Todos");
+    });
+   /* var db=client.db('TodoApp');
     console.log('Connected to MongoDB server');
     db.collection('Todos').insertOne({
         text:'Something to do',
@@ -29,6 +40,6 @@ MongoClient.connect('mongodb://localhost:27017/',(err,client)=>{
             
         }
         console.log(JSON.stringify(result.ops,undefined,2));
-    })
+    })*/
     client.close();
 })
