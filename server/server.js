941c9c68af72ecdfd12a2d41f132fe9fbca3eaa1
Anand <karanianandkumar@yahoo.com>
2018-04-27 18:56:44 +0530

    GET request using params

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -33,6 +34,27 @@ app.get('/todos', (req, res) => {
     })
 })
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({});
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({
+            todo,
+        })
+
+    }, (err) => {
+        res.status(400).send();
+    })
+
+
+})
 
 app.listen(3000, () => {
     console.log('Server is running on Port 3000');
