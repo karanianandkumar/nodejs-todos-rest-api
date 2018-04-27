b9b6fb8f335176f0e6973e48379d0b8e9b38a358
Anand <karanianandkumar@yahoo.com>
2018-04-27 18:24:01 +0530

    Test GET Request

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,14 +4,23 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+    text: 'learn flutter'
+},
+{
+    text: 'design flow for cstore'
+}]
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => {
         done();
     })
 })
 describe('POST /todos', () => {
     it('Should crete new Todo', (done) => {
-        var text = "Test todo text";
+        var text = "flutter";
 
         request(app)
             .post('/todos')
@@ -25,7 +34,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,7 +56,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => {
                     done(err);
@@ -55,3 +64,15 @@ describe('POST /todos', () => {
             })
     })
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done)
+    })
+})
