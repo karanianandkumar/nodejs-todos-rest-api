aef1b695866f2d04deb66dcd953a875e33e8776d
Anand Kumar <karanianandkumar@yahoo.com>
2018-05-08 22:32:07 +0530

    Test case for todo item

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID}= require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'learn flutter'
 },
 {
+    _id: new ObjectID(),
     text: 'design flow for cstore'
 }]
 
@@ -74,5 +77,31 @@ describe('GET /todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done)
+    })
+})
+
+describe('GET /todos/:id',()=>{
+    it('should return todo document',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 error',(done)=>{
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 error',(done)=>{
+        request(app)
+            .get(`/todos/12313233`)
+            .expect(404)
+            .end(done);
     })
 })
